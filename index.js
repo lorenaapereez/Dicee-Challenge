5545a5d28b5338eef92531391b50ad9f4437aed0
lorenaapereez <80012577+lorenaapereez@users.noreply.github.com>
2022-10-03 10:52:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const randomImgSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImgSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 wins!";
+  document.querySelector("h1").textContent = "Player 1 wins!";
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 wins!";
+  document.querySelector("h1").textContent = "Player 2 wins!";
 } else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h1").textContent = "Draw!";
 }
